bcea83755bdaa5e982b10880c4ee626db9238f85
francoax <francoa.duarte2001@gmail.com>
2023-02-13 23:34:25 -0300

    refactor form

--- src/main/webapp/js/formValidation.js
+++ src/main/webapp/js/formValidation.js
@@ -1,99 +1,109 @@
-/**
- * 
- */
- const validateForm = (formSelector) => {
-    const formElement = document.querySelector(formSelector);
+const validateForm = (formSelector) => {
+        const formElement = document.querySelector(formSelector);
 
-    const validationOptions = [
-        {
-            attribute : 'minlength',
-            isValid : input => input.value && input.value.length >= parseInt(input.minLength, 10),
-            errorMessage : (input, label) => `${label.textContent} debe tener almenos ${input.minLength} caracteres.`
-        },
-        {
-            attribute : 'custommaxlength',
-            isValid : input => input.value && input.value.length <= parseInt(input.getAttribute('custommaxlength'), 10),
-            errorMessage : (input, label) => `${label.textContent} no puede superar los ${input.getAttribute('custommaxlength')} caracteres.`
-        },
-        {
-            attribute : 'pattern',
-            isValid : input => {
-                const patternRegex = new RegExp(input.pattern);
-                return patternRegex.test(input.value);
+        const validationOptions = [
+            {
+                attribute : 'minlength',
+                isValid : input => input.value && input.value.length >= parseInt(input.minLength, 10),
+                errorMessage : (input, label) => `${label.textContent} debe tener almenos ${input.minLength} caracteres.`
             },
-            errorMessage : (input, label) => `No es un ${label.textContent} válido.`
-        },
-        {
-            attribute : 'match',
-            isValid : input => {
-                const matchSelector = input.getAttribute('match');
-                const matchedElement = document.querySelector(`#${matchSelector}`);
-                return matchedElement && matchedElement.value.trim() === input.value.trim()
+            {
+                attribute : 'custommaxlength',
+                isValid : input => input.value && input.value.length <= parseInt(input.getAttribute('custommaxlength'), 10),
+                errorMessage : (input, label) => `${label.textContent} no puede superar los ${input.getAttribute('custommaxlength')} caracteres.`
             },
-            errorMessage : (input, label) => {
-                const matchSelector = input.getAttribute('match');
-                const matchedElement = document.querySelector(`#${matchSelector}`);
-                const matchedLabel = matchedElement.parentElement.parentElement.parentElement.parentElement.querySelector('label');
+            {
+                attribute : 'pattern',
+                isValid : input => {
+                    const patternRegex = new RegExp(input.pattern);
+                    return patternRegex.test(input.value);
+                },
+                errorMessage : (input, label) => `No es un ${label.textContent} válido.`
+            },
+            {
+                attribute : 'match',
+                isValid : input => {
+                    const matchSelector = input.getAttribute('match');
+                    const matchedElement = document.querySelector(`#${matchSelector}`);
+                    return matchedElement && matchedElement.value.trim() === input.value.trim()
+                },
+                errorMessage : (input, label) => {
+                    const matchSelector = input.getAttribute('match');
+                    const matchedElement = document.querySelector(`#${matchSelector}`);
+                    const matchedLabel = matchedElement.parentElement.parentElement.parentElement.parentElement.querySelector('label');
 
-                return `${label.textContent} debe coincidir con ${matchedLabel.textContent}`
+                    return `${label.textContent} debe coincidir con ${matchedLabel.textContent}`
+                }
+            },
+            {
+                attribute : 'required',
+                isValid : input => input.value.trim() !== '',
+                errorMessage : (input, label) => `${label.textContent} es requerido.`
+            },
+        ];
+        
+        const validateSingleFormGroup = formGroup => {
+            const label = formGroup.querySelector('label');
+            const input = formGroup.querySelector('input');
+            const errorIcon = formGroup.querySelector('.error-icon')
+            const successIcon = formGroup.querySelector('.success-icon');
+            const helpContainer = formGroup.querySelector('.help');
+
+            let formGroupError = false;
+            for (let option of validationOptions) {
+                if(input.hasAttribute(option.attribute) && !option.isValid(input)){
+                    helpContainer.classList.add('is-danger');
+                    input.classList.add('is-danger');
+                    errorIcon.classList.remove('is-hidden');
+                    input.classList.remove('is-success');
+                    successIcon.classList.add('is-hidden');
+                    helpContainer.textContent = option.errorMessage(input, label);
+                    formGroupError = true;
+                }
             }
-        },
-        {
-            attribute : 'required',
-            isValid : input => input.value.trim() !== '',
-            errorMessage : (input, label) => `${label.textContent} es requerido.`
-        },
-    ];
-    
-    const validateSingleFormGroup = formGroup => {
-        const label = formGroup.querySelector('label');
-        const input = formGroup.querySelector('input');
-        const errorIcon = formGroup.querySelector('.error-icon')
-        const successIcon = formGroup.querySelector('.success-icon');
-        const helpContainer = formGroup.querySelector('.help');
-
-        let formGroupError = false;
-        for (let option of validationOptions) {
-            if(input.hasAttribute(option.attribute) && !option.isValid(input)){
-                helpContainer.classList.add('is-danger');
-                input.classList.add('is-danger');
-                errorIcon.classList.remove('is-hidden');
-                input.classList.remove('is-success');
-                successIcon.classList.add('is-hidden');
-                helpContainer.textContent = option.errorMessage(input, label);
-                formGroupError = true;
+            if(!formGroupError) {
+                helpContainer.classList.remove('is-danger');
+                input.classList.remove('is-danger');
+                errorIcon.classList.add('is-hidden');
+                helpContainer.textContent = '';
+                input.classList.add('is-success');
+                successIcon.classList.remove('is-hidden');
             }
-        }
-        if(!formGroupError) {
-            helpContainer.classList.remove('is-danger');
-            input.classList.remove('is-danger');
-            errorIcon.classList.add('is-hidden');
-            helpContainer.textContent = '';
-            input.classList.add('is-success');
-            successIcon.classList.remove('is-hidden');
-        }
-    }
 
-    formElement.setAttribute('novalidate', '');
+            return !formGroupError;
+        }
 
-    Array.from(formElement.elements).forEach(element => {
-        element.addEventListener('blur', event => {
-            validateSingleFormGroup(event.srcElement.parentElement.parentElement.parentElement.parentElement);
+        formElement.setAttribute('novalidate', '');
+
+        Array.from(formElement.elements).forEach(element => {
+            element.addEventListener('blur', event => {
+                validateSingleFormGroup(event.srcElement.parentElement.parentElement.parentElement.parentElement);
+            })
         })
-    })
 
-    formElement.addEventListener('submit', (e) => {
-        e.preventDefault();
-        validateAllFormGroups(formElement);
-    });
+        const validateAllFormGroups = formToValidate => {
+            const formGroups = Array.from(formToValidate.querySelectorAll('.formGroup'));
+            return formGroups.every(formGroup => validateSingleFormGroup(formGroup));
+        };
 
-    const validateAllFormGroups = formToValidate => {
-        const formGroups = Array.from(formToValidate.querySelectorAll('.formGroup'))
-        formGroups.forEach(formGroup => {
-            validateSingleFormGroup(formGroup);
-    })
-}
+        formElement.addEventListener('submit', (e) => {
+            const formValid = validateAllFormGroups(formElement);
+
+            if(!formValid){
+                e.preventDefault();
+            } else {
+                console.log('form valid');
+            }
+        });
 };
 
+/* const sendForm = (formElement) => {
+    const formObject = Array.from(formElement.elements)
+        .filter(element => element.type !== 'submit')
+        .reduce((accumulator, element) => ({...accumulator, [element.id] : element.value}), {});
+    
+    console.log(formObject);
+} */
 
-validateForm('#signupForm');
+
+validateForm('#signupForm')
